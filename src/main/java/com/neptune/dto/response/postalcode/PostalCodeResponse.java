436e2ade08ba7c130b43a3908535d61b1a965cc7
Edison Martinez <martinezfriasedison@gmail.com>
2024-06-20 17:56:32 -0400

    Agregando pruebas unitarias

--- src/main/java/com/neptune/dto/response/postalcode/PostalCodeResponse.java
+++ src/main/java/com/neptune/dto/response/postalcode/PostalCodeResponse.java
@@ -2,7 +2,10 @@ package com.neptune.dto.response.postalcode;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.Setter;
+
 @Getter
+@Setter
 public class PostalCodeResponse {
     private  boolean error;
     private String  message;
